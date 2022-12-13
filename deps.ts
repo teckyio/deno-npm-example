28bed5287b5ff598776452d4b02146269e1dbcfa
Gordon Lau Wai Chung <gordon@tecky.io>
2022-12-13 15:18:06 +0000

    Added Deno NPM Project

---
+++ deps.ts
@@ -0,0 +1,14 @@
+
+
+export { default as express } from 'npm:express'
+
+export type { Request, Response, Express } from 'npm:@types/express'
+
+export { default as pg } from 'npm:pg'
+
+export type {Client } from 'npm:@types/pg'
+
+export { default as dotenv } from 'npm:dotenv'
+
+
+
---
+++ index.ts
@@ -0,0 +1,30 @@
+import { Client, Express, express, pg, dotenv } from "./deps.ts";
+
+dotenv.config()
+
+const app:Express = express()
+const config = {
+    database: Deno.env.get('POSTGRES_DB'),
+    user: Deno.env.get('POSTGRES_USER'),
+    password: Deno.env.get('POSTGRES_PASSWORD')
+}
+const client: Client = new pg.Client(config)
+
+await client.connect()
+
+
+app.get('/todos', async(req, res)=>{
+    const data = (await client.query("SELECT * from todos")).rows
+    res.json(data)
+})
+
+app.post('/todos',async (req ,res)=>{
+    const content = req.body.content
+    await client.query("INSERT INTO todos (content) values ($1)",[content])
+    res.json("Hello World")
+})
+
+
+app.listen(8080, ()=>{
+    console.log("Listening at http://localhost:8080")
+})
